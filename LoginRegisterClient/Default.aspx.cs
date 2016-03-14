da50aaf55bf899a73116f6103dd43743289b288b
Mark Pfaff <markdpfaff@gmail.com>
2016-03-14 11:59:29 -0700

    Initial Commit

---
+++ LoginRegisterClient/Default.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void LoginButton_Click(object sender, EventArgs e)
+    {
+        VenueLogin();
+    }
+
+    protected void VenueLogin()
+    {
+        LoginRegisterServiceReference.STLoginServiceClient src =
+            new LoginRegisterServiceReference.STLoginServiceClient();
+        int key = src.VenueLogin(PasswordTextbox.Text, UserNameTextbox.Text);
+        if(key != -1)
+        {
+            ErrorLabel.Text = "Welcome";
+            Session["userKey"] = key;
+            Response.Redirect("AddShow.aspx");
+        }
+        else
+        {
+            ErrorLabel.Text = "Login Fail";
+        }
+    }
+}
---
+++ LoginRegisterClient/VenueRegistration.aspx.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class VenueRegistration : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+       
+    }
+
+    protected void SubmitButton_Click(object sender, EventArgs e)
+    {
+        VenueLoginServiceReference.STLoginServiceClient src =
+            new VenueLoginServiceReference.STLoginServiceClient();
+        VenueLoginServiceReference.VenueLite vLite =
+            new VenueLoginServiceReference.VenueLite();
+
+        vLite.VenueName = VenueNameTextBox.Text;
+        vLite.venueEmail = EmailTextBox.Text;
+        vLite.venueAddress = AddressTextBox.Text;
+        vLite.venueCity = CityTextBox.Text;
+        vLite.venueState = StateTextBox.Text;
+        vLite.venueZipCode = ZipTextBox.Text;
+        vLite.venuePhone = PhoneTextBox.Text;
+        vLite.venueWebPage = WebPageTextBox.Text;
+        if (AgeTextBox.Text != null)
+        {
+            vLite.venueAgeRestriction = int.Parse(AgeTextBox.Text.ToString());
+        }
+        vLite.venueLoginUserName = UsernameTextBox.Text;
+        vLite.venuLoginPasswordPlain = AddressTextBox.Text;
+
+
+        int result = src.VenueRegistration(vLite);
+
+        if(result != -1)
+        {
+            ResultLabel.Text = "Successfully Registered";
+        }
+        else
+        {
+            ResultLabel.Text = "Registration Failed";
+        }
+    }
+}
